4d7c8d07f3f8492e0b117c785ff94ac1e459fd04
Ryan McNierney <ryan.mcnierney@gmail.com>
2019-08-14 23:54:21 -0400

    Add standings model

---
+++ db/models/standings.js
@@ -0,0 +1,59 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Standings = db.define('standings', {
+  teamId: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    references: {
+      model: 'teams',
+      key: 'id'
+    }
+  },
+  team_id: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  record: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  win: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  loss: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  tie: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  top_six_win: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  top_six_loss: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  total_win: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  total_loss: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  pts_for: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  pts_against: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+})
+
+module.exports = Standings
