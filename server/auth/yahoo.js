bfb590374fd7786e3eae405f340375889f094f04
Ryan McNierney <ryan.mcnierney@gmail.com>
2019-08-15 23:17:40 -0400

    Add /auth/yahoo/refresh token route

--- server/auth/yahoo.js
+++ server/auth/yahoo.js
@@ -1,30 +1,41 @@
 const router = require('express').Router()
 const { Token } = require('../../db/models')
 const axios = require('axios')
+const qs = require('qs')
 module.exports = router
 
 router.get('/refresh', async (req, res) => {
-  // // get refresh token
-  // const tokenData = await Token.findOne({
-  //   where: { id: 1 },
-  //   attributes: ['refresh_token']
-  // })
-  // const refreshToken = tokenData.dataValues.refresh_token
+  try {
+    // get new access token
+    const config = {
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Credentials': true
+      },
+      auth: {
+        username: process.env.YAHOO_CLIENT_ID,
+        password: process.env.YAHOO_CLIENT_SECRET
+      }
+    }
+    const data = {
+      'grant_type': 'refresh_token',
+      'redirect_uri': 'oob',
+      'refresh_token': process.env.YAHOO_REFRESH_TOKEN
+    }
+    const refreshUrl = 'https://api.login.yahoo.com/oauth2/get_token'
+    const yahooResponse = await axios.post(refreshUrl, qs.stringify(data), config)
+    const { access_token, refresh_token } = yahooResponse.data
 
-  // // get new access token
-  // const yahooResponse = await axios({
-  //   method: 'post',
-  //   url: 'https://api.login.yahoo.com/oauth2/get_token',
-  //   auth: {
-  //     username: process.env.YAHOO_CLIENT_ID,
-  //     password: process.env.YAHOO_CLIENT_SECRET
-  //   },
-  //   data: {
-  //     grant_type: 'refresh_token',
-  //     redirect_uri: 'oob',
-  //     refresh_token: refreshToken
-  //   }
-  // })
+    // update the database
+    await Token.update({ access_token, refresh_token }, {
+      where: { id: 1 },
+      returning: true,
+      plain: true
+    })
 
-  // console.log('yahooResponse', yahooResponse)
+    // respond with new access_token
+    res.send(access_token)
+  } catch (err) {
+    console.log(err)
+  }
 })
