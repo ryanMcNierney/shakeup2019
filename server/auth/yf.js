053b620a766ec4af97b5c3217eb736c6d877f720
Ryan McNierney <ryan.mcnierney@gmail.com>
2019-08-22 20:34:31 -0400

    Add /api/yahoo/scoreboard route

--- server/auth/yf.js
+++ server/auth/yf.js
@@ -45,4 +45,24 @@ const yfGetStandings = async (getToken, refreshToken) => {
   }
 }
 
-module.exports = { yfGetTeams, yfGetStandings }
+const yfGetScoreboard = async (getToken, refreshToken) => {
+  try {
+    const accessToken = await getToken()
+    await yf.setUserToken(accessToken)
+    const data = await yf.league.scoreboard(process.env.YF_LEAGUE_KEY)
+    return data
+  } catch (err) {
+    try {
+      const newAccessToken = await refreshToken()
+      await yf.setUserToken(newAccessToken)
+      const data = await yf.league.scoreboard(process.env.YF_LEAGUE_KEY)
+
+      return data
+    } catch (error) {
+      console.log('Another err', error)
+      return error
+    }
+  }
+}
+
+module.exports = { yfGetTeams, yfGetStandings, yfGetScoreboard }
