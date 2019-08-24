26b88dc244ca90dc2fc285aca7e91ac767e218c0
Ryan McNierney <ryan.mcnierney@gmail.com>
2019-08-24 09:07:26 -0400

    Add topSix model

---
+++ db/models/topSix.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const TopSixes = db.define('top_six', {
+  id: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    primaryKey: true
+  },
+  current_week: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  team_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  total: {
+    type: Sequelize.DECIMAL,
+    allowNull: false
+  },
+  top_six: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  }
+})
+
+module.exports = TopSixes
