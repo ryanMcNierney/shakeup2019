4cd82f3078e40bd3e8981846475dcf8967478ee9
Ryan McNierney <ryan.mcnierney@gmail.com>
2019-08-29 23:02:49 -0400

    Add react

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  mode: isDev ? 'development' : 'production',
+  entry: [
+    '@babel/polyfill', // enables async-await
+    './client/index.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map',
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}
