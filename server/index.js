08d4c9ea24189f9a842b6f78100fc997df5b0f0b
Ryan McNierney <ryan.mcnierney@gmail.com>
2019-09-05 21:01:53 -0400

    Updated server catch-all

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')))
 // routing
 app.use('/api', require('./api'))
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public/index.html'))
 })
 
